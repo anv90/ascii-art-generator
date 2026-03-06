ad3f01f7a04f2739b19bcc8dc191bce228406cfb
Anvesha Kumar <anv90@users.noreply.github.com>
2026-03-05 16:52:50 -0800

    added some psuedocode

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+app.post (because we want to have a longer message)
+    body:
+        message - str
+        sprite - bool
+    if sprite
+        method that gets the sprite and the message -> cowsay
+        return json
+    else 
+        method that turns the text into ascii ->text2art
+        return json str
+
+
+"""
